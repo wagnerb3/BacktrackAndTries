f6c68c203df8db59972a79771dfc6fe2f4b74b9a
got2b <wagnerb@udel.edu>
2020-04-20 12:12:07 -0400

    Moved TrieNode to Solution.java for easier implementation

--- Backtrack/src/main/Solution.java
+++ Backtrack/src/main/Solution.java
@@ -1,6 +1,8 @@
 package main;
+
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
@@ -54,7 +56,7 @@ public class Solution {
 		return "0";
 	}
 
-	//Part 1
+	// Part 1
 	// Checks to see if morsed word is in Dictionary
 	public static void handleSpacedLetters(String morsed) {
 		String result = findWord(morsed);
@@ -73,9 +75,9 @@ public class Solution {
 		w.removeIf(a -> !a.startsWith(s));
 		return w;
 	}
-	
-	//Part 2
-	//Uses Backtracking to find words
+
+	// Part 2
+	// Uses Backtracking to find words
 	public static void findWords(String morse, String current) {
 		ArrayList<String> possibilities = filter(new ArrayList<>(DICTIONARY), current);
 		if (possibilities.size() == 0) {
@@ -93,7 +95,7 @@ public class Solution {
 
 	public static void handleWord(String morsed) {
 		findWords(morsed, "");
-		for(String word : WORD) {
+		for (String word : WORD) {
 			System.out.println(word);
 		}
 	}
@@ -131,6 +133,59 @@ public class Solution {
 			break;
 		}
 
+	}
+
+	class TrieNode {
+		HashMap<Character, TrieNode> children;
+		String word;
+
+		public void addWord(TrieNode root, String newWord) {
+			TrieNode current = root;
+			for (char letter : newWord.toCharArray()) {
+				if (!current.children.containsKey(letter)) {
+					current.children.put(letter, new TrieNode());
+				}
+				current = current.children.get(letter);
+			}
+			current.word = newWord;
+		}
+
+		public boolean hasWord(TrieNode root, String goal) {
+			TrieNode current = root;
+			for (char letter : goal.toCharArray()) {
+				if (current.children.containsKey(letter)) {
+					current = current.children.get(letter);
+				} else {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public ArrayList<String> getAllWords(TrieNode root) {
+			ArrayList<String> result = new ArrayList<>();
+			if (root.word != null) {
+				result.add(root.word);
+			}
+			for (char letter : root.children.keySet()) {
+				ArrayList<String> childWords = getAllWords(root.children.get(letter));
+				result.addAll(childWords);
+			}
+			Collections.sort(result);
+			return result;
+		}
+
+		public ArrayList<String> autocomplete(TrieNode root, String start) {
+			TrieNode current = root;
+			for (char letter : start.toCharArray()) {
+				if (current.children.containsKey(letter)) {
+					current = current.children.get(letter);
+				} else {
+					return new ArrayList<String>();
+				}
+			}
+			return current.getAllWords(current);
+		}
 	}
 
 }
