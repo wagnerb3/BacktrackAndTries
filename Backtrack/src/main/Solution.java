33bf2ee8acb50eb9a8c5c04a4b91ef63faff0f56
got2b <wagnerb@udel.edu>
2020-04-20 23:52:24 -0400

    Moved TrieNode to Solution.java and implemented part 3

--- Backtrack/src/main/Solution.java
+++ Backtrack/src/main/Solution.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
@@ -10,12 +11,13 @@ import java.io.FileReader;
 import java.lang.String;
 
 public class Solution {
-	private static HashMap<String, String> MORSEMAP = new HashMap<>();
-	private static ArrayList<String> DICTIONARY = new ArrayList<>();
-	private static ArrayList<String> MORSE = new ArrayList<>();
-	private static int MAX = 6;
-	private static ArrayList<String> WORD = new ArrayList<>();
-	private static TrieNode TRIE = new TrieNode();
+	private static HashMap<String, String> MORSEMAP = new HashMap<>(); // Maps every letter to a morse code sequence
+	private static HashMap<String, String> CHARMAP = new HashMap<>(); // Maps every morse code sequence to a letter
+	private static ArrayList<String> DICTIONARY = new ArrayList<>(); // Contains all the words in the dictionary file
+	private static ArrayList<String> MORSE = new ArrayList<>(); // Contains all the morse code sequences
+	private static final int MAX = 6; // Is the length of longest morse code sequence
+	private static ArrayList<String> WORD = new ArrayList<>(); // Holds possible words in backtracking part
+	private static TrieNode TRIE = new TrieNode(); // Is the Trie of the Dictionary file
 
 	// Loads Morse Code and Dictionary from files
 	public static void loadFiles() {
@@ -27,6 +29,7 @@ public class Solution {
 			while ((line = reader.readLine()) != null) {
 				String[] letters = line.split(" ");
 				MORSEMAP.put(letters[1], letters[0]);
+				CHARMAP.put(letters[0], letters[1]);
 				MORSE.add(letters[1]);
 			}
 			reader.close();
@@ -40,6 +43,7 @@ public class Solution {
 			System.err.format("Exception occurred trying to read '%s'.", filename);
 			e.printStackTrace();
 		}
+		addDictionary();
 	}
 
 	// Finds word in Dictionary
@@ -93,6 +97,7 @@ public class Solution {
 		}
 	}
 
+	// Main function for part 2 that prints out words
 	public static void handleWord(String morsed) {
 		findWords(morsed, "");
 		for (String word : WORD) {
@@ -100,12 +105,55 @@ public class Solution {
 		}
 	}
 
-	public static void handleSpacedWords(String morsed) {
-		String[] words = morsed.split(" ");
-		for (String word : words) {
+	// Returns an equivalent of the word in morse code
+	// Used for adding dictionary to TRIE
+	public static String charToMorse(String word) {
+		String result = "";
+		for (char letter : word.toCharArray()) {
+			result += CHARMAP.get(Character.toString(letter));
+		}
+		return result;
+	}
+
+	// Used for part 3
+	// Adds all words to the TRIE
+	public static void addDictionary() {
+		for (String word : DICTIONARY) {
 			TrieNode.addWord(TRIE, word);
 		}
-		System.out.println("INCOMPLETE PART 3");
+	}
+
+	//Part 3 - Uses a trie to print out
+	//spaced word possibilities
+	public static void handleSpacedWords(String morsed) {
+		ArrayList<ArrayList<String>> options = new ArrayList<>();
+		ArrayList<Integer> lens = new ArrayList<>();
+		String[] words = morsed.split(" ");
+		for (String word : words) {
+			ArrayList<String> parts = TrieNode.autocomplete(TRIE, word);
+			options.add(parts);
+			lens.add(parts.size());
+		}
+		int phraseNum = 1;
+		for (int len : lens) {
+			phraseNum *= len;
+		}
+		ArrayList<String> phrases = new ArrayList<>();
+		for (int i = 0; i < phraseNum; i++) {
+			phrases.add("");
+		}
+		for (ArrayList<String> arr : options) {
+			int split = arr.size();
+			int count = 0;
+			while(count < phraseNum) {
+				phrases.set(count, phrases.get(count)+arr.get(count%split)+" ");
+				count++;
+			}
+		}
+		for(String p : phrases) {
+			p = p.trim();
+			System.out.println(p);
+		}
 	}
 
 	public static void handleSentence(String morsed) {
@@ -138,6 +186,57 @@ public class Solution {
 			break;
 		}
 
+	}
+
+	static class TrieNode {
+
+		HashMap<Character, TrieNode> children = new HashMap<>();
+		ArrayList<String> words = new ArrayList<>();
+
+		public static void addWord(TrieNode root, String newWord) {
+			TrieNode current = root;
+			String morsed = charToMorse(newWord);
+			for (char letter : morsed.toCharArray()) {
+				if (!current.children.containsKey(letter)) {
+					current.children.put(letter, new TrieNode());
+				}
+				current = current.children.get(letter);
+			}
+			current.words.add(newWord);
+		}
+
+		public static boolean hasWord(TrieNode root, String goal) {
+			TrieNode current = root;
+			for (char letter : goal.toCharArray()) {
+				if (current.children.containsKey(letter)) {
+					current = current.children.get(letter);
+				} else {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public static ArrayList<String> getAllWords(TrieNode root) {
+			ArrayList<String> result = new ArrayList<>();
+			if (!root.words.isEmpty()) {
+				result.addAll(root.words);
+			}
+			Collections.sort(result);
+			return result;
+		}
+
+		public static ArrayList<String> autocomplete(TrieNode root, String start) {
+			TrieNode current = root;
+			for (char letter : start.toCharArray()) {
+				if (current.children.containsKey(letter)) {
+					current = current.children.get(letter);
+				} else {
+					return new ArrayList<String>();
+				}
+			}
+			return getAllWords(current);
+		}
 	}
 
 }
